06f2ddc733768a964749d64f635d1afa00a0a5ea
hydhym <339291883@qq.com>
2019-07-12 16:51:25 +0800

    Add files via upload

---
+++ apps.php
@@ -0,0 +1,5 @@
+为了方便大家测试登录接口 拉下代码  我写了个测试页面 调用方式 打开hb 在谷歌浏览器运行 ，请求url加上'', 其他参数可以直接用&拼接，post方式加上method=POST
+http://127.0.0.1:8080/#/pages/test/testApi?api_url='apps/index.php?url=cart_vip/list'
+
+
+http://127.0.0.1:8080/#/pages/test/testApi?api_url='apps/index.php?url=cart_vip/list'&method=POST
